ccd8015111e282cc7c77364c80ac2c58107ebed4
oleksii kozyrev <ak.kozyrev01@gmail.com>
2024-10-29 17:01:55 -0700

    proj structure upgradet

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React, { useState } from 'react';
-import Header from './header/Header';
-import ChatMeButton from './header/ChatMeButton';
-import ContactButton from './header/ContactButton';
-import ProjectsButton from './header/ProjectsButton';
-import AboutSection from './components/AboutSection';
-import ResumeSection from './components/ResumeSection';
-import CalculatorModal from './components/CalculatorModal';
-import Footer from './components/Footer';
-import FallenHearts from './components/FallenHearts';
-import ChatBot from './components/ChatBot';
-import ChatButton from './components/ChatButton';
-import AboutButton from './header/AboutButton';
+import Header from './components/header/Header';
+import AboutSection from './components/aboutSection/AboutSection';
+import ResumeSection from './components/resumeSection/ResumeSection';
+import CalculatorModal from './components/calculator/CalculatorModal';
+import Footer from './components/footer/Footer';
+import FallenHearts from './components/fallenHearts/FallenHearts';
+import ChatBot from './components/chatBot/ChatBot';
+import ChatButton from './components/chatButton/ChatButton';
+
 
 // Объявляем типы для пропсов
 const App: React.FC = () => {
@@ -22,20 +19,13 @@ const App: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center bg-gray-100 min-h-screen">
-      <Header />
-      <AboutSection />
-      <AboutButton />
-      <ChatMeButton />
-      <ContactButton />
-      <ProjectsButton />
       <FallenHearts />
+      <Header />
       <AboutSection />
       <ResumeSection />
       <CalculatorModal />
       <Footer />
-      {/* Кнопка открытия/закрытия чата */}
       <ChatButton onClick={toggleChat} isChatOpen={isChatOpen} />
-      {/* Отображаем чат, только если он открыт */}
       {isChatOpen && <ChatBot />}
     </div>
   );
