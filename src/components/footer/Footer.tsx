3b08eec67e95f23e43309463ec7651df4f520509
Evgeny <evgeny.buturlia@gmail.com>
2024-12-04 00:12:41 +0400

    removed social icons

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,24 +1,30 @@
 // src/components/Footer/Footer.tsx
-import React, { forwardRef } from 'react';
-import SocialIcons from './SocialIcons';
+import React, { forwardRef } from "react";
 
-interface FooterProps { 
+interface FooterProps {
   isCompact?: boolean;
   windowSize: { width: number; height: number };
 }
 
-const Footer = forwardRef<HTMLDivElement, FooterProps>(({ windowSize }, ref) => {
-  return (
-    <footer ref={ref} className="w-full bg-black text-white py-4 flex flex-col items-center">
-      <SocialIcons />
-      <p className="text-xs mt-2">© 2024 Oleksii Kozyrev. All rights reserved.</p>
-      <p className="text-xs mt-2">The idea to create this website was inspired by my friend and front-end developer, Eugene.</p>
-    </footer>
-  );
-});
+const Footer = forwardRef<HTMLDivElement, FooterProps>(
+  ({ windowSize }, ref) => {
+    return (
+      <footer
+        ref={ref}
+        className="w-full bg-black text-white py-4 flex flex-col items-center"
+      >
+        <p className="text-xs mt-2">
+          © 2024 Oleksii Kozyrev. All rights reserved.
+        </p>
+        <p className="text-xs mt-2">
+          The idea to create this website was inspired by my friend and
+          front-end developer, Eugene.
+        </p>
+      </footer>
+    );
+  }
+);
 
 Footer.displayName = "Footer";
 
 export default Footer;
-
-
