1be9487ab15ae373af4819726e3548b0276f9535
oleksii kozyrev <ak.kozyrev01@gmail.com>
2024-12-06 15:34:48 -0800

    style of icons changed

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -12,7 +12,9 @@ const Footer = forwardRef<HTMLDivElement, FooterProps>(
   ({ windowSize}, ref) => {
     return (
       <footer ref={ref} className="w-full bg-black text-white py-4 flex flex-col items-center">
+        <div className="flex gap-4" >
            {footerList.map((contact)=>{return(<IconCard contact={contact}/>)})}
+        </div>
         <p className="text-xs mt-2">
           © 2024 Oleksii Kozyrev. All rights reserved.
         </p>
--- src/components/footer/FooterList.tsx
+++ src/components/footer/FooterList.tsx
@@ -3,44 +3,35 @@ import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { faInstagram, faLinkedin, faGithub, faFacebook, faTelegram, faGoogle} from '@fortawesome/free-brands-svg-icons';
 
 export interface Contact {
-    name: string;
     href: string;
     icon: IconDefinition;
 }
-
 export const footerList: Contact[] = [
   {
-    name: 'instagram',
     href: 'https://www.instagram.com/leorjini',
     icon: faInstagram,
 },
 {
-    name: 'linkedin',
     href: 'https://www.linkedin.com/in/oleksii-kozyrev-106b37261',
     icon: faLinkedin,
 },
 {
-    name: 'github',
     href: 'https://github.com/0leks11',
     icon: faGithub,
 },
 {
-    name: 'facebook',
     href: 'https://www.facebook.com/faa.hfvptc',
     icon: faFacebook,
 },
 {
-    name: 'telegram',
     href: 'https://t.me/Leorjini',
     icon: faTelegram,
 },
 {
-    name: 'google',
     href: 'mailto:ak.kozyrev01@gmail.com',
     icon: faGoogle,
 },
 // {
-//     name: 'upwork',
 //     href: 'https://www.upwork.com/freelancers/~0148d6dc0e1c6905ee',
 //     icon: faUpwork,
 // },
