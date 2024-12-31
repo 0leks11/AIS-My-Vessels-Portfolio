93a03295f55ffb5def7c42a7913cd1c170343282
oleksii kozyrev <ak.kozyrev01@gmail.com>
2024-12-30 22:09:44 -0800

    ActiveButton component parent hover props state transfered to components

--- src/components/aboutSection/ActiveButton.tsx
+++ src/components/aboutSection/ActiveButton.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode } from "react";
+import React, { FC, ReactNode, useState } from "react";
 import Arrow from "../resumeSection/Arrow";
 
 interface ActiveButtonProps {
@@ -14,16 +14,23 @@ export const ActiveButton: FC<ActiveButtonProps> = ({
   icon,
   href,
 }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <a
       href={href}
       target="_blank"
       rel="noopener noreferrer"
       onClick={onClick}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
       className="flex justify-between border-2 bg-neutral-900 hover:bg-neutral-800 border-zinc-600 h-55 w-full p-4 transition-colors no-underline"
     >
       {button}
       <Arrow icon={icon} />
+      {React.cloneElement(button as React.ReactElement, {
+        isParentHovered: isHovered,
+      })}
     </a>
   );
 };
